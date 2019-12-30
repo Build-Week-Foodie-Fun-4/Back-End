38cde97785b47b6c128594e8a3ea36ac222e854e
Rasha Moumneh <rasha@fastmail.com>
2019-12-30 17:53:52 -0500

    Add users model

---
+++ auth/auth-router.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const validateRegistration = require("../middleware/register-middleware");
+const Users = require("../users/users-model");
+const signToken = require("../helpers/signToken");
+
+router.get("/users", async (req, res) => {
+  let allUsers = await Users.find();
+  try {
+    if (allUsers) {
+      res.status(200).json(allUsers);
+    } else {
+      res.status(404).json("No users to retrieve");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post("/register", validateRegistration, (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json("incorrect");
+    });
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = signToken(user);
+        res.status(200).json({
+          token,
+          message: `Welcome ${user.username}!`
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json("error");
+    });
+});
+
+module.exports = router;
---
+++ users/users-model.js
@@ -0,0 +1,27 @@
+const db = require("../database/db-config");
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById
+};
+
+function find() {
+  return db("users");
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
+async function add(user) {
+  const [id] = await db("users").insert(user);
+
+  return findById(id);
+}
