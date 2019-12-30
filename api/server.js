a771137c0fabfc5522a56403d97874f81228c047
Rasha Moumneh <rasha@fastmail.com>
2019-12-30 17:41:37 -0500

    Server up and running

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("Up and Running!");
+});
+
+module.exports = server;
