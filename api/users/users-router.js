c386a58b02347f3b564aa2a53cd1d234d25d8462
Rasha Moumneh <rasha@fastmail.com>
2020-01-06 17:08:32 -0500

    Add get user by id endpoint

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -15,6 +15,19 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  let user = await Users.findById(req.params.id);
+  try {
+    if (user) {
+      res.status(200).json("user");
+    } else {
+      res.status(404).json("No user with that id found");
+    }
+  } catch {
+    res.status(500).json("Error retrieving user");
+  }
+});
+
 router.delete("/:id", (req, res) => {
   Users.remove(req.params.id)
     .then(count => {
