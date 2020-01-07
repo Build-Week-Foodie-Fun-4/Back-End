5688426dac682ab0261414a3d0f59f91ca13777b
Rasha Moumneh <rasha@fastmail.com>
2020-01-07 12:59:20 -0500

    Added CORS middleware to server.js

--- api/server.js
+++ api/server.js
@@ -16,6 +16,15 @@ server.use(
     allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 server.options("*", cors());
 server.use(express.json());
 server.use("/auth", authRouter);
